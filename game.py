c8c8357360e2e571ab7cf8cd193e994e8e03034c
Sebik9000 <159123960+Sebik9000@users.noreply.github.com>
2024-04-15 18:03:54 +0200

    fix int

--- game.py
+++ game.py
@@ -14,7 +14,7 @@ vitez = max(skore, key=lambda x: skore[x])
 
 #Definivání funkce kostky která používá modul random aby generovala čísla (mezi 1,6) funkce vrátí-(odevzdá) hodnotu tohoto hodu
 def hod_kostkou():
-    return random.randit(1,6)
+    return random.randint(1,6)
 
 #Definování funkce bodování která vezme ty čísla co nahoře kód vyhodil a vrátí-(odevzdá) jejich hodnotu, ale u jedničky odevzdá 100 a u 6 odevzdá 60.
 def body(cislo):
